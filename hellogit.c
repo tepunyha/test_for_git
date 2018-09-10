cd6e6805a0781a95fefb1f814302cb6553c1518f
YC_YANG <ycyang@ntu.edu.tw>
2018-09-10 18:23:21 +0800

    qvvdv
    dqeq23323

--- hellogit.c
+++ hellogit.c
@@ -6,7 +6,7 @@ int main {
   printf("Hello github test_branch in master branch 004_test2");
 
 
-	printf("6th rebase test in tqst branchdabd");
+	printf("7th rebase test in thdabd");
 
 
   return 0;
