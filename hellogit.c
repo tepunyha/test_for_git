6b92f2735a406adffa52fa9939452bb220a49428
ycyang <tepunyha@gmail.com>
2021-03-05 16:10:55 +0800

    v0.1n

--- hellogit.c
+++ hellogit.c
@@ -1,6 +1,8 @@
 #incldue <stdio.h>
 #include <string.h>
 
+/* version 0.1n */
+
 int main {
 
   printf("Hello github test_branch in master branch 004_test2");
@@ -8,6 +10,7 @@ dfad
 
 	printf("11d again in tdfadfasdvanch");
 
+  printf("move to test new");
 
   return 0;
 }
